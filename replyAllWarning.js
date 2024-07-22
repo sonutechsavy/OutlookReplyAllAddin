56c4864dd372d2251053bc57a7328c449e6f19d7
sonutechsavy <sonu@techsavy.pro>
2024-07-22 15:03:56 -0700

    1.0.0.1
    code updates

--- replyAllWarning.js
+++ replyAllWarning.js
@@ -1,18 +1,20 @@
-Office.initialize = function (reason) {
-    $(document).ready(function () {
-        Office.context.mailbox.item.addHandlerAsync(Office.EventType.ItemChanged, itemChanged);
-    });
-};
-
-function itemChanged(eventArgs) {
-    if (Office.context.mailbox.item.itemType === Office.MailboxEnums.ItemType.Message) {
-        var message = Office.context.mailbox.item;
-        if (message.conversationId && message.conversationId.replyAllRecipients.length > 1) {
-            Office.context.mailbox.displayNewMessageForm({
-                toRecipients: [],
-                subject: "Warning: Reply All",
-                body: "Are you sure you want to reply all?"
-            });
-        }
+Office.onReady((info) => {
+    if (info.host === Office.HostType.Outlook) {
+      // Add any initialization logic to be run once the host is ready
     }
-}
+  });
+  
+  function showReplyAllWarning(event) {
+    const item = Office.context.mailbox.item;
+    if (item.itemType === Office.MailboxEnums.ItemType.Message) {
+      if (item.toRecipients.length > 1) {
+        Office.context.mailbox.displayNewMessageForm({
+          toRecipients: [],
+          subject: "Warning: Reply All",
+          body: "Are you sure you want to reply all?"
+        });
+      }
+    }
+    event.completed();
+  }
+  
